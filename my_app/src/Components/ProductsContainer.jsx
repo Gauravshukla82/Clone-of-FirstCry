19dc560d5ce57f6961b9fcf0d80e97bd70a3e124
Gauravshukla82 <gauravshukla.ec1022@gmail.com>
2023-03-31 23:54:22 +0530

    day-4 footer , navbar dropdown , add to cart

---
+++ my_app/src/Components/ProductsContainer.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import ProductCard from "./ProductCard";
+import style from "./ProductsContainer.module.css"
+function ProductsContainer() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `http://localhost:8080/products`
+    )
+      .then((res) => res.json())
+      .then((product) => {
+          console.log(product);
+          setData(product)
+      });
+  },[]);
+  return (
+    <div style={{display:"flex",width:"90%",margin:"auto"}}>
+      <div className={style.filter_section}>
+        {/* filter sections here */}
+      </div>
+      <div className={style.product_container}>
+        
+        {
+          data?.map((el) =>  ( <ProductCard {...el} />))
+        }
+      </div>
+    </div>
+  );
+}
+
+export default ProductsContainer;
---
+++ my_app/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import BigCardComponent from '../components/BigCardComponent'
+import FooterPage from './FooterPage'
+import PosterGirlBoy from '../components/PosterGirlBoy'
+
+import Slider from "../components/carsole/Slider"
+const Hom = () => {
+  return (
+   <>
+
+       <Slider/> 
+       <BigCardComponent/>
+       <PosterGirlBoy/>
+      <FooterPage/> 
+   </>
+  )
+}
+
+export default Hom
